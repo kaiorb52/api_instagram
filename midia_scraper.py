15491253e0f241559bee4630394d7722278039aa
Kaio R. Barbara <kaio.rb@hotmail.com>
2024-10-29 19:54:53 -0300

    ajustes finais para implementação no servidor

--- midia_scraper.py
+++ midia_scraper.py
@@ -7,6 +7,10 @@ from datetime import timedelta, datetime
 from constants import dia_atual,ontem, run
 from functions import format_date
 
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+from send_to_bigquery import *
 
 def midia_sraper(
   client, 
@@ -15,30 +19,41 @@ def midia_sraper(
   data = (datetime.today() - timedelta(days = 1)).strftime("%Y-%m-%d"), 
   daily = True):
     
-  midia_var  = midia + '_list'
-  dados      = df[midia_var]
-  df_perma   = pd.DataFrame()
-  inicio     = datetime.today()
-  dia_semana = dia_atual.weekday()
-  i          = 0
+  midia_var   = midia + '_list'
+  dados       = df[midia_var]
+  df_perma    = pd.DataFrame()
+  inicio      = datetime.today()
+  dia_semana  = dia_atual.weekday()
+  i           = 0
   
   print("post de antes de:", data)
   print("tempo de inicio:", inicio)
   print(f"raspando {midia}...")
   
+  credentials  = service_account.Credentials.from_service_account_file('bq_auth.json')
+  project_id   = 'uc3m-autoridades'
+  dataset_id   = 'monitoramento'
+  table_name   = f'prefeitos_scrapper_{midia_var}'
+  path         = f'{project_id}.{dataset_id}.{table_name}'
+  bq_mode      = 'overwrite'
+  
+  if os.path.exists('data/id.csv'):
+    ids_salvos = pd.read_csv(ids_path)['ID'].unique()
+    df = df[~df['ID'].isin(ids_salvos)]
+  
   if midia == "instagram":
     n_post     = 300
     api        = "shu8hvrXbJbY3Eb9W"
-    time_var   = "timestamp"
+    #time_var   = "timestamp"
   
   if midia == "facebook":
     n_post     = 200
     api        = "KoJrdxJCTtpon81KY"
-    time_var   = "time"
+    #time_var   = "time"
   
   if daily == True and dia_semana == 0:
     data = (datetime.today() - timedelta(days = 2)).strftime("%Y-%m-%d") 
-    
+  
   for idx, candidato in enumerate(dados):
     #for candidato in dados:
     candidato_id = df.loc[idx, 'ID']
@@ -64,7 +79,6 @@ def midia_sraper(
           'timestamp', 'ownerFullName', 'ownerUsername', 'ownerId'
         ]
 
-
       if midia == "facebook":
         run_input = {
           "startUrls": [{ "url": url}],       # Url do candidato
@@ -74,48 +88,52 @@ def midia_sraper(
         colunas = [
           'CandidatoID'
         ]
-
       
       # Chamando a API e obtendo os resultados
       run = client.actor(api).call(run_input = run_input)
 
       # Iterando sobre os itens retornados pela API
       for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-
           df_temp  = pd.DataFrame(columns=colunas)
           df_temp  = pd.DataFrame([item])
           df_temp['CandidatoID'] = candidato_id
 
           df_perma = pd.concat([df_perma, df_temp], ignore_index=True)
           
-      time.sleep(2)
+      time.sleep(3)
       
-      if i == 50:
+      if i == 3:
         i = 0
-        time.sleep(100)
-      
+        
+        results   = df_perma[colunas]
+        results_f = pd.merge(
+          df,             
+          results,                
+          left_on    = 'ID',
+          right_on   = 'CandidatoID',  
+          how        = 'left'      
+        )
+        #upload_bigquery(credentials, project_id, path, results_f, bq_mode)
+        if ('data' in os.listdir()) == False:
+          os.mkdir('data')
+        pd.DataFrame(results_f['ID'].unique(), columns=['ID']).to_csv(f'data/id.csv', index=False)
+        
+        df_temp    = pd.DataFrame()
+        df_perma   = pd.DataFrame()
+        results_f  = pd.DataFrame()
+        time.sleep(5)
+
   results   = df_perma[colunas]
-  
   results_f = pd.merge(
-    df,             
-    results,                
+    df,
+    results,
     left_on    = 'ID',
     right_on   = 'CandidatoID',  
     how        = 'left'      
   )
-          
-  # credentials = service_account.Credentials.from_service_account_file('bq_auth.json')
-
-  # project_id = 'uc3m-autoridades'
-  # dataset_id = 'monitoramento'
-  # table_name = 'prefeitos_scrapper_facebook'
-  # path = f'{project_id}.{dataset_id}.{table_name}'
-
-  # upload_bigquery(credentials, project_id, path, results_final, 'overwrite')
+  #upload_bigquery(credentials, project_id, path, results_f, bq_mode)
+  if ('data' in os.listdir()) == False:
+    os.mkdir('data')
+  pd.DataFrame(results_f['ID'].unique(), columns=['ID']).to_csv(f'data/id.csv', index=False)
   
-  # if ('data' in os.listdir()) == False:
-  #  os.mkdir('data')
-  # results_f.to_csv(f'data/{midia}_{inicio}.csv', index=False)
-
   print(f"tempo final da raspasgem do {midia}:", datetime.today())
-
